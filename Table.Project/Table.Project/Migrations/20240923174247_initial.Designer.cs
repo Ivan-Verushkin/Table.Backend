25b4fa8ef1e9c3247a7e3d91498be43abf63b3df
ivan verushkin <ivanveruskin86@gmail.com>
2024-09-23 20:45:07 +0300

    Migrations updated to fit more players

--- Table.Project/Table.Project/Migrations/20240923174247_initial.Designer.cs
+++ Table.Project/Table.Project/Migrations/20240923174247_initial.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Table.Project.Data;
 
@@ -10,9 +11,11 @@ using Table.Project.Data;
 namespace Table.Project.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240923174247_initial")]
+    partial class initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
